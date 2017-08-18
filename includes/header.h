7821252e8c7ed634a1b12dc619833cb4876606b4
Noe WEEKS <nweeks@e3r9p6.42.fr>
2017-08-18 10:19:11 +0200

    Added function to read first line

--- includes/header.h
+++ includes/header.h
@@ -6,7 +6,7 @@
 /*   By: nweeks <nweeks@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/18 09:54:57 by nweeks            #+#    #+#             */
-/*   Updated: 2017/08/18 09:56:01 by nweeks           ###   ########.fr       */
+/*   Updated: 2017/08/18 10:18:36 by nweeks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
+
+typedef struct	s_map
+{
+	char		vide;
+	char		obstacle;
+	char		plein;
+	int			hauteur;
+	int			largeur;
+}				t_map;
 
 #endif
--- srcs/libft.c
+++ srcs/libft.c
@@ -1,36 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utility.c                                          :+:      :+:    :+:   */
+/*   libft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nweeks <nweeks@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/18 09:55:26 by nweeks            #+#    #+#             */
-/*   Updated: 2017/08/18 10:18:34 by nweeks           ###   ########.fr       */
+/*   Created: 2017/08/18 10:10:56 by nweeks            #+#    #+#             */
+/*   Updated: 2017/08/18 10:18:43 by nweeks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
-#define BUFF_SIZE 500000
 
-t_map	*get_first_line(int fd)
+char	ft_read_int(int fd, int *number)
 {
-	t_map	*first_line;
-	char	junk;
+	char	c;
 
-	first_line = malloc(sizeof(t_map));
-	first_line->vide = ft_read_int(fd, &first_line->hauteur);
-	read(fd, &first_line->obstacle, 1);
-	read(fd, &first_line->plein, 1);
-	read(fd, &junk, 1);
-	return (first_line);
-}
-
-int		ft_get_fd(char *file)
-{
-	int	fd;
-
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
-		
+	while ((read(fd, &c, 1)) && c >= '0' && c <= '9')
+		*number = *number * 10 + c - 48;
+	return (c);
 }
