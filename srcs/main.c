df11b4860f19484edd1180cec9696f1a3a9144ec
Noe WEEKS <nweeks@e3r9p2.42.fr>
2017-08-23 22:46:20 +0200

    Added management if too many lines

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nweeks <nweeks@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/18 09:51:08 by nweeks            #+#    #+#             */
-/*   Updated: 2017/08/23 22:19:45 by nweeks           ###   ########.fr       */
+/*   Updated: 2017/08/23 22:46:04 by nweeks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	ft_start_bsq(int fd)
 		return ;
 	}
 	matrix = ft_parse(fd, map);
-	if (!matrix || (fd != 0 && read(fd, &c, 1) > 0))
+	if (!matrix || (read(fd, &c, 1) > 0 && c != '\n'))
 	{
 		ft_putstr("map error\n", 2);
 		return ;
